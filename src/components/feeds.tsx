24f2f7ff3d9588d94cd80c9646d559aaaef2d026
unknown <abdulaleemuibrahim@gmail.com>
2025-01-23 22:11:02 -0800

    fix

--- src/components/feeds.tsx
+++ src/components/feeds.tsx
@@ -63,69 +63,73 @@ const Feeds: React.FC<FeedsProps> = ({ projects, students }) => {
         </select>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {filteredProjects.map((project) => {
-          const student = students.find(
-            (student) => student.matricNumber === project.studentMatric
-          );
-          return (
-            <Link
-              key={project.studentMatric}
-              href={`/dashboard/projects/${project.studentMatric}`}>
-              <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer h-80 flex flex-col justify-between overflow-hidden border border-gray-200 hover:border-blue-500">
-                <div>
-                  <h2 className="text-2xl font-semibold mb-3 text-blue-600">
-                    {project.title}
-                  </h2>
-                  <p className="text-gray-800 mb-4 line-clamp-3">
-                    {project.description}
-                  </p>
-                </div>
-                {student && (
-                  <div className="bg-gray-100 p-4 rounded-lg mt-4">
-                    <div className="flex flex-col space-y-2">
-                      <div>
-                        <p className="text-gray-600">
-                          Student:{" "}
-                          <span className="font-medium">{student.name}</span>
-                        </p>
-                        <p className="text-gray-600">
-                          Matric Number:{" "}
-                          <span className="font-medium">
-                            {student.matricNumber}
-                          </span>
-                        </p>
-                      </div>
-                      <div>
-                        <p className="text-gray-600">
-                          Dept:{" "}
-                          <span className="font-medium">
-                            {student.department}
-                          </span>
-                        </p>
-                        <p className="text-gray-600">
-                          Date:{" "}
-                          <span className="font-medium">
-                            {project.date &&
-                              new Date(project.date).toLocaleDateString(
-                                "en-US",
-                                {
-                                  year: "numeric",
-                                  month: "long",
-                                  day: "numeric",
-                                }
-                              )}
-                          </span>
-                        </p>
+      {filteredProjects.length === 0 ? (
+        <div className="text-center text-gray-600 mt-4 ">No projects found</div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {filteredProjects.map((project) => {
+            const student = students.find(
+              (student) => student.matricNumber === project.studentMatric
+            );
+            return (
+              <Link
+                key={project.studentMatric}
+                href={`/dashboard/projects/${project.studentMatric}`}>
+                <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer h-80 flex flex-col justify-between overflow-hidden border border-gray-200 hover:border-blue-500">
+                  <div>
+                    <h2 className="text-2xl font-semibold mb-3 text-blue-600">
+                      {project.title}
+                    </h2>
+                    <p className="text-gray-800 mb-4 line-clamp-3">
+                      {project.description}
+                    </p>
+                  </div>
+                  {student && (
+                    <div className="bg-gray-100 p-4 rounded-lg mt-4">
+                      <div className="flex flex-col space-y-2">
+                        <div>
+                          <p className="text-gray-600">
+                            Student:{" "}
+                            <span className="font-medium">{student.name}</span>
+                          </p>
+                          <p className="text-gray-600">
+                            Matric Number:{" "}
+                            <span className="font-medium">
+                              {student.matricNumber}
+                            </span>
+                          </p>
+                        </div>
+                        <div>
+                          <p className="text-gray-600">
+                            Dept:{" "}
+                            <span className="font-medium">
+                              {student.department}
+                            </span>
+                          </p>
+                          <p className="text-gray-600">
+                            Date:{" "}
+                            <span className="font-medium">
+                              {project.date &&
+                                new Date(project.date).toLocaleDateString(
+                                  "en-US",
+                                  {
+                                    year: "numeric",
+                                    month: "long",
+                                    day: "numeric",
+                                  }
+                                )}
+                            </span>
+                          </p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )}
-              </div>
-            </Link>
-          );
-        })}
-      </div>
+                  )}
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
